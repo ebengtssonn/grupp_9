f893e77062017f8a6ea8f05bf88cfc727cdc302a
Anna2an <90031744+Anna2an@users.noreply.github.com>
2022-01-19 13:50:27 +0100

    Variabler

---
+++ labb_1.py
@@ -0,0 +1,10 @@
+#Labb1 variabler
+from inspect import FrameInfo
+
+
+friaStudietimmar = 40 
+pythonTimmar = 20
+
+#Skriver ut värden för variablerna i en mening.
+print("Vi har", friaStudietimmar, "h i veckan för att studera.")
+print("*************************************")
