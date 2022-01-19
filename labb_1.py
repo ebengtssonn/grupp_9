7bb2e959dcd3c0123c732ff9673d9fdbdc22d7e7
ebengtssonn <h21emben@du.se>
2022-01-19 16:05:54 +0100

    Update labb_1.py

--- labb_1.py
+++ labb_1.py
@@ -5,6 +5,10 @@ from inspect import FrameInfo
 friaStudietimmar = 40 
 pythonTimmar = 20
 
-#Skriver ut värden för variablerna i en mening.
+#Skriver ut värdet för variabeln friaStudietimmar i en mening.
 print("Vi har", friaStudietimmar, "h i veckan för att studera.")
 print("*************************************")
+
+#Skriver ut båda värdena för variablerna friaStudietimmar och pythonTimmar i en mening.
+print("Av dessa", friaStudietimmar, "h kommer vi använda", pythonTimmar, "h till att programera python.")
+print("****************************************************************")
