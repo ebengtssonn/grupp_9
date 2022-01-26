967b48da4072a72e5c85ed84c4acf0e48976a844
ebengtssonn <h21emben@du.se>
2022-01-26 14:34:33 +0100

    forbrukning

---
+++ labb_2.py
@@ -0,0 +1,9 @@
+bransleTank = int(input("Hur stor är bilens bränsletank? ange i hela liter:"))
+stracka = int(input("Hur långt kan du åka på en tank? ange i hela mil:"))
+
+forbrukning = bransleTank / stracka
+
+
+miljoBil = 0.5
+
+print(bool(miljoBil))
