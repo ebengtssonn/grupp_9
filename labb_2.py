a2eda812dc7e94526ad03f8ed38b6b1e5434a4d1
Anna2an <90031744+Anna2an@users.noreply.github.com>
2022-01-26 15:50:56 +0100

    Update labb_2.py

--- labb_2.py
+++ labb_2.py
@@ -1,9 +1,15 @@
+#Input från användaren.
+#Bränsletankens storlek. Datatyp integer.
 bransleTank = int(input("Hur stor är bilens bränsletank? ange i hela liter:"))
+
+#Sträcka per tank. Datatyp integer.
 stracka = int(input("Hur långt kan du åka på en tank? ange i hela mil:"))
 
+#Räknar ut förbrukningen genom att dividera liter med mil.
+#Här blir förbrukningen av datatypen integer.
 forbrukning = bransleTank / stracka
 
+#Skriver ut bilens förbrukning och anger om det är en miljöbil eller inte med hjälp av boolean.
+print(f" Din bil drar {forbrukning} liter per mil. Klassas den som miljöbil: ", bool(forbrukning <=0.5))
+#Typar om förbrukningen till boolean.
 
-miljoBil = 0.5
-
-print(bool(miljoBil))
