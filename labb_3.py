8348541a6c92c8f19b85def0fc3418e2295f64ab
Anna2an <90031744+Anna2an@users.noreply.github.com>
2022-02-02 12:51:36 +0100

    Update labb_3.py

--- labb_3.py
+++ labb_3.py
@@ -2,38 +2,44 @@ import random
 stickor = 21 #Totalt 21st stickor från början.
 run = True
 
+print(f"Det finns {stickor} stickor att spela med")
+
 while run:
-    if stickor > 0:
-        print(f"Det finns {stickor} stickor kvar")
     
-
-    drag1 = int(input("Hur många stickor vill du ta? En eller två?"))
+    #print(f"Det finns {stickor} stickor kvar")
+    drag = int(input("Hur många stickor vill du ta? En eller två?"))
    
-
-    if drag1 > 2:
-        drag1 = 2
+    if drag > 2:
+        drag = 2
+        print("Du valde ett för högt nummer, det avrundas till två")
     
-    elif drag1 < 1:
-        drag1 = 1
-    stickor = stickor - (drag1)
-    
+    elif drag < 1:
+        drag = 1
+        print("Du valde ett för lågt nummer, det avrundas till ett")
+    stickor = stickor - drag
+   
 
     if stickor <= 0:
       run = False 
       print("Det var sista stickan, användaren förlorade!")
       break
+    print(f"Det finns {stickor} stickor kvar")
+
     if stickor > 2:
         dator = random.randint(1,2)
     else:
         dator = 1
-        print(f"Datorn tog {dator}")
+    print(f"Datorn tog {dator}")
 
-stickor = stickor - (dator)
-print(f"Det finns {stickor} stickor kvar")
+    stickor = stickor - dator
+    print(f"Det finns {stickor} stickor kvar")
 
-if stickor <= 0:
-        run = False 
-        print("Det var sista stickan, användaren förlorade!")
+    if stickor <= 0:
+        run = False
+        print("Det var sista stickan, datorn förlorade!")
+
+
+        
         
     
     
