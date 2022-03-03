e2f9d904e222c7ab1191ec9a1e4e7a181307f7cf
Anna2an <90031744+Anna2an@users.noreply.github.com>
2022-03-03 12:01:33 +0100

    uppdatering

--- Labb_7/labb_7.py
+++ Labb_7/labb_7.py
@@ -1,37 +1,37 @@
-from tkinter import *
-import shopping
-import funktioner
-
-
+from tkinter import * #importerar tkinter
+import shopping # importerar klassenfilen shopping
+ 
+#variabler med varor som finns med shoppinglistan från början
 vara1 = shopping.Shopping('mjölk',5,15)
 vara2 = shopping.Shopping('ägg',12,10)
 vara3 = shopping.Shopping('bröd',20,20)
 
-shopping_dict = {1 : vara1, 2 : vara2, 3 : vara3} #Variabeln shoppinglista innehåller listan med objekten(varorna).
+shopping_dict = {1 : vara1, 2 : vara2, 3 : vara3} #Variabeln shoppinglista innehåller dict med objekten(varorna).
 
 
-id=3
+id=3 #anger start id
 #Funktion 1
 def addera_func():#add funktion
-    global id
-    id += 1 # id = id+1
-    namn = e_namn.get()
-    antal = int(e_antal.get())
-    pris = int(e_pris.get())
-    #print(namn, antal,pris)#skriver ut för att testa i terminal
-    vara = shopping.Shopping(namn, antal, pris)
-    shopping_dict[id] = vara
-    print(shopping_dict[id])
-    t_area.insert(END, shopping_dict[id])#skriver ut en specifik plats
+    t_area.delete('1.0', END) #rensar innan 
+    global id #global variabel id
+    id += 1 # id = id+1 för att kunna lägga till vara
+    namn = e_namn.get() #variabel för att kunna ta emot angivet namn
+    antal = int(e_antal.get()) # variabel för att kunna ta emot angivet antal, typar om till int
+    pris = int(e_pris.get()) #variabel för att kunna ta emot pris, typar om till int
+    vara = shopping.Shopping(namn, antal, pris) # varaibel för att hämta objektet vara från klassfilen
+    shopping_dict[id] = vara # varan tilldelas sitt id i dict
+    t_area.insert(END,'Du har lagt till varan: \n') #skriver ut
+    t_area.insert(END, shopping_dict[id])#skriver ut 
 
 #Funktion 2
-def antal_func():#add funktion
-    t_area.insert(END, len(shopping_dict))#skriver ut en specifik plats
+def antal_func():#funktion för antal varor
+    t_area.delete('1.0', END) #rensar innan 
+    t_area.insert(END, len(shopping_dict))#skriver ut antal varor i dict med hjälp av len
 
 #Funktion 3
-def info_func():
+def info_func(): #funktion för 
     t_area.delete('1.0', END) #rensar innan 
-    sokt_vara = e_info.get()
+    sokt_vara = e_namn.get()
     for v in shopping_dict.values():
         if sokt_vara.lower() in v.getNamn().lower():
             t_area.insert(END, v)
@@ -52,18 +52,36 @@ def helaListan_func():
 #funktion 6 
 def uppdatera_func():
     t_area.delete('1.0', END) #rensar innan
+    nytt_pris = int(e_pris.get())
+    nytt_antal = int(e_antal.get())
+    vara = e_namn.get()
+    for i in shopping_dict.values():
+        if vara == e_namn.get():
+            i.setAntal(nytt_antal)
+            i.setPris(nytt_pris)
+            break
+    t_area.insert (END,'Nu är varan uppdaterad: \n')
+    t_area.insert (END, i)
+
+#funktion 7
+def ta_bort_func():
+    t_area.delete('1.0', END) #rensar innan 
+    id = int(e_id.get())
+    del shopping_dict[id]
+    t_area.insert (END, f'Du tog bort nr: {id}')
+   
 
 root = Tk()#instans av klassen
 root.title('Shopping')
 
-lbl_namn = Label(root, text = 'Fyll i vara:')#namn lbl, klass Label , Text ='' detsamma som print
+lbl_namn = Label(root, text = 'Fyll i namn på vara:')#namn lbl, klass Label , Text ='' detsamma som print
 lbl_namn.grid(row = 0, column = 0)
 lbl_antal = Label(root, text = 'Fyll i antal:')#namn lbl, klass Label , Text ='' detsamma som print
 lbl_antal.grid(row = 1, column = 0)
 lbl_pris = Label(root, text = 'Fyll i pris:')#namn lbl, klass Label , Text ='' detsamma som print
 lbl_pris.grid(row = 2, column = 0)
-lbl_info = Label(root, text = 'Sök namn på vara:')
-lbl_info.grid(row = 3, column = 0)
+lbl_id = Label(root, text = 'Ange ID för att ta bort vara:')
+lbl_id.grid(row = 3, column = 0)
 
 e_namn = Entry(root, width=40, borderwidth=5)#klassen entry har skapats med objektet e_namn
 e_namn.grid(row=0, column=1)
@@ -71,24 +89,28 @@ e_antal = Entry(root, width=40, borderwidth=5)#klassen entry har skapats med obj
 e_antal.grid(row=1, column=1)
 e_pris = Entry(root, width=40, borderwidth=5)#klassen entry har skapats med objektet e_namn
 e_pris.grid(row=2, column=1)
-e_info = Entry(root, width=40, borderwidth=5)#klassen entry har skapats med objektet e_namn
-e_info.grid(row=3, column=1)
+e_id = Entry(root, width=40, borderwidth=5)#klassen entry har skapats med objektet e_namn
+e_id.grid(row=3, column=1)
 
 
 
 btn_addera = Button(root, text='Lägg till', padx=30, pady=20, command=addera_func)
-btn_addera.grid(row=4, column=0)
+btn_addera.grid(row=4, column=0, columnspan=1)
 btn_antal = Button(root, text='Antal varor', padx=30, pady=20, command=antal_func)
-btn_antal.grid(row=4, column=1)
+btn_antal.grid(row=4, column=1, columnspan=1)
 btn_sok = Button(root, text='Visa info (sök på namn)', padx=30, pady=20, command=info_func)
-btn_sok.grid(row=4, column=2)
+btn_sok.grid(row=4, column=2, columnspan=1)
 btn_namn = Button(root, text='Visa namn på alla varor)', padx=30, pady=20, command=namn_func)
-btn_namn.grid(row=4, column=3)
+btn_namn.grid(row=5, column=0, columnspan=1)
 btn_helaListan = Button(root, text='Visa hela listan', padx=30, pady=20, command=helaListan_func)
-btn_helaListan.grid(row=4, column=4)
+btn_helaListan.grid(row=5, column=1, columnspan=1)
+btn_uppdatera = Button(root, text='Uppdatera (pris/antal)', padx=30, pady=20, command=uppdatera_func)
+btn_uppdatera.grid(row=5, column=2, columnspan=1)
+btn_ta_bort = Button(root, text='Ta bort  (ange ID)', padx=30, pady=20, command=ta_bort_func)
+btn_ta_bort.grid(row=5, column=3, columnspan=1)
 
 t_area = Text(root, height=100, width=60)
-t_area.grid(row=5, column=0, columnspan=3)
+t_area.grid(row=6, column=0, columnspan=3)
 
 
 
